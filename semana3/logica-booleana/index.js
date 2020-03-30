864ecd1f9d80719e163a6e0ce0ff4e06433c6a18
Kalleby Gomes <kallebyfg@gmail.com>
2020-03-30 16:58:41 -0300

    Atividade tarde

---
+++ semana3/logica-booleana/index.js
@@ -0,0 +1,37 @@
+/* 
+Exercícios de interpretação de código
+1.
+a. false
+b. false
+c. true
+d. false
+e. boolean
+
+2.
+a. Arrays são maneiras de armazenar e acessar mais de uma informação simultaneamente. 
+Declarando em JS: "let array = [1, 2, 3]"
+
+b. Inicial é 0
+
+c. Digita-se: "array.length"
+
+d.
+I. undefined
+II. null
+III. 11
+IV. 3 e 4
+V. 19 e 9
+VI. 3
+VII. 1 (mas pensei que era 2, não entendi) 
+*/
+
+
+// Exercícios de escrita de código
+
+// 1.
+// a.
+let f = prompt("Informe os graus em Fahrenheit:")
+let converterFK = (f - 32)*5/9 + 273.15
+console.log(f+" °F (Fahrenheit) são "+converterFK+" K (Kelvin)")
+
+// b.
